38729666c6248fc57eb004bc67873ac58b2a30b3
Stiina Riihinen <167634661+cstri001@users.noreply.github.com>
2025-01-17 13:47:22 +0200

    Delete hasheri.js

--- hasheri.js
+++
@@ -1,7 +0,0 @@
-// tällä minä vain teen sen salasanan sinne tietokantaan hashattuna
-// kun sen haluaa nähdä tuolla terminaalissa, niin kirjoita "node hasheri.js"
-
-let hashi = require('crypto').createHash('SHA512').update('kissakala').digest('hex')
-
-console.log(hashi)
-
